ec5d61787e91daae31b189ada5713ecc8042bf5c
Seang-G <mgw7730@gmail.com>
2022-05-11 15:10:08 +0900

    `22. 5.11.
    - 추가 -
    도서관 위치 표시

---
+++ JS/dotsControl.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll("#buttons button");
+
+const publicButton = buttons[0];
+const litleButton = buttons[1];
+
+function toggleHidden(nodes){
+  for (const node of nodes){
+    node.classList.toggle("hidden");
+  }
+}
+
+function publicButtonHandler(){
+  toggleHidden(publicDots);
+}
+
+function litleButtonHandler(){
+  toggleHidden(litleDots);
+}
+
+publicButton.addEventListener("click", publicButtonHandler);
+litleButton.addEventListener("click", litleButtonHandler);
