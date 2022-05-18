36ebe65ebc3faa503b1884c46c93d613b0145757
Seang-G <mgw7730@gmail.com>
2022-05-18 16:16:49 +0900

    `22. 5.18.
    - 추가 -
    detail 페이지

---
+++ JS/details.js
@@ -0,0 +1,23 @@
+const wrap = document.querySelector("#main-wrap");
+
+const table = document.createElement("table");
+const thead = document.createElement("thead");
+const tr = document.createElement("tr");
+tr.innerHTML = "<th>도서관명</th><th>전화번호</th><th>도로명</th>";
+thead.appendChild(tr);
+table.appendChild(thead);
+
+const tbody = document.createElement("tbody");
+var json = $.getJSON("../../천안시_도서관.json", function(jsonData){
+  $.each(jsonData, function(_, data){
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<td>${data.도서관명}</td><td>${data.도서관전화번호}</td><td>${data.소재지도로명주소}</td>`;
+    tbody.appendChild(tr);
+    console.dir(data);
+  });
+});
+table.appendChild(tbody);
+
+table.setAttribute("border", "3px");
+
+wrap.appendChild(table);
--- JS/dotsControl.js
+++
@@ -1,5 +0,0 @@
-// const d = document.getElementsByClassName("dot");
-// console.log(d);
-// for (const v of d){
-//   console.log(v);
-// }
